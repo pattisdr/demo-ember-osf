a7895d545f5cab22bbe0bc76986fc6bc770dca53
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-02-01 21:11:58 -0500

    Add tangible mixin.

--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 import NodeActionsMixin from 'ember-osf/mixins/node-actions';
+import TaggableMixin from 'ember-osf/mixins/taggable-mixin';
 
 
-export default Ember.Controller.extend(NodeActionsMixin, {
+
+export default Ember.Controller.extend(NodeActionsMixin, TaggableMixin, {
     actions: {
         editProject(title, description) {
             return this.send('updateNode', title, description);
